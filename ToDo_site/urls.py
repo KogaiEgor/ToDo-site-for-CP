72039c6a4352dfa99e2bc054105009a959e8b1c8
Egor <kogayegor@gmail.com>
2023-02-24 20:04:14 +0300

    Сделал регистрацию

--- ToDo_site/urls.py
+++ ToDo_site/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #Аутентификация
+    path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    #Список дел
+    path('current/', views.CurrentToDo, name='CurrentToDo'),
+    path('', views.home, name='home'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+#superusers name - red_ranger
+#password - o711begor
+
+def home(request):
+    return render(request, 'todo/home.html')
+def signupuser(request):
+    if request.method == "GET":
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('CurrentToDo')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',
+                              {'form': UserCreationForm(), 'error': 'Это имя пользователя уже используется'})
+        else:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':'Пароли не совпадают'})
+
+def logoutuser(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('home')
+
+def loginuser(request):
+    if request.method == "GET":
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Неправильный логин или пароль'})
+        else:
+            login(request, user)
+            return redirect('CurrentToDo')
+def CurrentToDo(request):
+    return render(request, 'todo/CurrentToDo.html')
